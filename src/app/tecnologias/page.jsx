94d924dec99348f14bf283709d6e2c26953ff8f4
anderlehmann <andersonvlehmann@gmail.com>
2024-02-01 21:14:49 -0300

    criando a pagina tecnologias

---
+++ src/app/tecnologias/page.jsx
@@ -0,0 +1,30 @@
+import "./conhecimentos.css"
+
+import IconsTechs from "@/components/IconsTechs";
+import Header from "@/components/Header";
+
+export default function Conhecimentos() {
+  return (
+    <main className="App">
+      <Header />
+      <div className="body-page body-conhecimentos">
+        <p id="p-techs">Estas são as tecnologias que utilizo e já utilizei em alguns projetos!</p>
+        <div id="wrapper-techs">
+          <IconsTechs techName="API Rest" caminho="/images/api.png" site="https://www.w3schools.com/js/js_api_intro.asp" />
+          <IconsTechs techName="Arquitetura de Camadas" caminho="/images/camadas.png" site="https://www.ibm.com/br-pt/topics/three-tier-architecture" />
+          <IconsTechs techName="CSS" caminho="/images/css-3.png" site="https://www.w3schools.com/css" />
+          <IconsTechs techName="Docker" caminho="/images/docker.png" site="https://www.docker.com" />
+          <IconsTechs techName="HTML" caminho="/images/html-5.png" site="https://www.w3schools.com/html" />
+          <IconsTechs techName="Javascript" caminho="/images/javascript.png" site="https://www.w3schools.com/html" />
+          <IconsTechs techName="MySQL" caminho="/images/mysql.png" site="https://www.w3schools.com/html" />
+          <IconsTechs techName="Next JS" caminho="/images/nextjs.png" site="https://www.w3schools.com/html" />
+          <IconsTechs techName="Node JS" caminho="/images/node.png" site="https://www.w3schools.com/html" />
+          <IconsTechs techName="Orientação a Objetos" caminho="/images/oop.png" site="https://www.w3schools.com/html" />
+          <IconsTechs techName="React" caminho="/images/react.svg" site="https://www.w3schools.com/html" />
+          <IconsTechs techName="Redux" caminho="/images/redux.png" site="https://www.w3schools.com/html" />
+          <IconsTechs techName="Sequelize ORM" caminho="/images/sequelize.svg" site="https://www.w3schools.com/html" />
+        </div>
+      </div>
+    </main>
+  )
+};
