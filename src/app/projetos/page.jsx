b3f554a95038faff02660bc97d6c0e2c6c5d07a5
anderlehmann <andersonvlehmann@gmail.com>
2024-07-29 16:44:21 -0300

    inicio projeto shoes store

--- src/app/projetos/page.jsx
+++ src/app/projetos/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Header from "@/components/Header";
+import Link from "next/link";
 
 export default function Projetos() {
   return (
@@ -8,6 +9,7 @@ export default function Projetos() {
       <Header />
       <div className="body-page">
         <h1>Projetos</h1>
+        <Link href='/projetos/shoes-store'>Shoes Store</Link>
       </div>
     </main>
   )
