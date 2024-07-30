28d7ad3b4a427fd4ae02ad9f362d682460bc94e6
anderlehmann <andersonvlehmann@gmail.com>
2024-07-30 20:04:11 -0300

    tst

--- src/app/projetos/solado-store/HeaderShoesStore.jsx
+++ src/app/projetos/solado-store/HeaderShoesStore.jsx
@@ -9,19 +9,19 @@ export function HeaderShoesStore() {
     <header id='header-store'>
       <Link href='/projetos/solado-store'>Solado®</Link>
       <div id='icons-header'>
-        <Image width={25} height={25} id="search-desktop" className='icons-header-store' src="/images/search-desktop.svg" alt="pesquisar" />
-        <Image width={25} height={25} id="search-mobile" className='icons-header-store' src="/images/search-mobile.svg" alt="pesquisar" />
+        <img width={25} height={25} id="search-desktop" className='icons-header-store' src="./images/search-desktop.svg" alt="pesquisar" />
+        <img width={25} height={25} id="search-mobile" className='icons-header-store' src="./images/search-mobile.svg" alt="pesquisar" />
 
         <Link href="/projetos/solado-store/usuario" className='icons-header-store'>
-          <Image width={25} height={25} src="/images/cart.svg" alt="carrinho" />
+          <img width={25} height={25} src="./images/cart.svg" alt="carrinho" />
         </Link>
 
         <Link href="/projetos/solado-store/usuario" className='icons-header-store'>
-          <Image width={25} height={25} src="/images/like.svg" alt="favoritos" />
+          <img width={25} height={25} src="./images/like.svg" alt="favoritos" />
         </Link>
 
         <Link href="/projetos/solado-store/usuario" className='icons-header-store'>
-          <Image width={25} height={25} src="/images/user.svg" alt="usuário" />
+          <img width={25} height={25} src="./images/user.svg" alt="usuário" />
         </Link>
       </div>
     </header>
