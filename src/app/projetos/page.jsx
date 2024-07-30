43a0611f46001c3accdff6e3ac98a230330cf118
anderlehmann <andersonvlehmann@gmail.com>
2024-07-30 18:44:41 -0300

    adiciona projeto na aba de projetos

--- src/app/projetos/page.jsx
+++ src/app/projetos/page.jsx
@@ -3,13 +3,18 @@
 import Header from "@/components/Header";
 import Link from "next/link";
 
+import './projetos.css';
+
 export default function Projetos() {
   return (
     <main className="App">
       <Header />
-      <div className="body-page">
-        <h1>Projetos</h1>
-        <Link href='/projetos/shoes-store'>Shoes Store</Link>
+      <div className="body-page body-projetos">
+        <a href='/projetos/solado-store' className='projects'>
+          <p id='over-message'>WIP...</p>
+          <img id='img-shoes-store' src="/images/project-shoes-store.jpg" alt="" />
+          <p>Shoes Store</p>
+        </a>
       </div>
     </main>
   )
