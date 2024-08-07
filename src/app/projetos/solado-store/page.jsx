2e64b171797d56cf28c189b81f6321cc5a38ba74
anderlehmann <andersonvlehmann@gmail.com>
2024-08-07 18:40:38 -0300

    adiciona loading e fetch para a api

--- src/app/projetos/solado-store/page.jsx
+++ src/app/projetos/solado-store/page.jsx
@@ -3,33 +3,51 @@
 import './shoesStore.css'
 import { HeaderShoesStore as Header } from './HeaderShoesStore';
 import { IconShoes } from '@/components/shoes-store/IconShoes';
+import { useEffect, useState } from 'react';
 
-export default function Projetos() {
+export default function HomeShoesStore() {
+  const [dataShoes, setDataShoes] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3090/shoes');
+      const result = await response.json();
+      setDataShoes(result);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error.message)
+    }
+  };
+
+  const dataMapped = () => {
+    if (dataShoes.length > 0) {
+      return dataShoes.map((shoe, index) => {
+        return <IconShoes
+          key={index}
+          src={shoe.image_url}
+          brand={shoe.brand}
+          model={shoe.model}
+          price={shoe.price}
+        />
+      })
+    }
+    return <p className='p-home-load-error'>Não foi possível carregar os dados.</p>
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='main-background'>
       <div id='main-content'>
         <Header />
         <section id='icons-shoes-wrapper'>
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
-          <IconShoes />
+          {/* <div id='icons-shoes-wrapper-center'> */}
+          {isLoading ? <p className='p-home-load-error'>Carregando...</p> : dataMapped()}
+          {/* </div> */}
         </section>
       </div>
     </div>
--- src/components/shoes-store/IconShoes.jsx
+++ src/components/shoes-store/IconShoes.jsx
@@ -4,19 +4,20 @@ import Image from 'next/image';
 
 import './IconShoes.css';
 
-export function IconShoes() {
+export function IconShoes({ src, brand, model, price }) {
   return (
     <article id='box-icon-shoes'>
       <Image
-        src='https://res.cloudinary.com/dsgkcgx1s/image/upload/v1722625879/shoesAdidas_by1ixm.jpg'
+        src={src}
         alt='tenis'
         id='image-icon-shoes'
         width={200}
         height={200}
+        priority
       />
       <div id='model-price-box'>
-        <p>Adidas Advantage Base</p>
-        <p>R$ 599,00</p>
+        <p>{`${brand} ${model}`}</p>
+        <p>R$ {price}</p>
       </div>
     </article>
   )
